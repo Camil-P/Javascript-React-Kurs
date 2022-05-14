4c11f99e458c408ec0ebdc4be9a94955be755c0a
Camil Plojovic <camil.plojovic@levi9.com>
2022-05-14 18:04:30 +0200

    Added Javascript/Variables

---
+++ Javascript_Basics/Variables/index.js
@@ -0,0 +1,23 @@
+// Klasicni brojevi
+var x = 5;
+var y = 3.4;
+var res = x + y;
+console.log(res);
+
+// String (tekst) i broj
+var stringBroj = '23';
+var rezultat = stringBroj + 4;
+console.log(rezultat);
+
+// String (tekst) i String
+var nekiString = "Neki String,";
+var drugiString = " Neki drugi string";
+console.log(nekiString + drugiString);
+
+// Aritmeticke Operacije
+var firstNumber = 2.3;
+var secondNumber = 0.4;
+var aritmethicResult = (firstNumber+4)/secondNumber;
+console.log(aritmethicResult);
+
+
