228bae55332fd392164bec68819bc4ec321d4b56
Camil Plojovic <camil.plojovic@levi9.com>
2022-05-17 20:32:19 +0200

    Added BeginnerFunctions folder

---
+++ Javascript_Basics/BeginnerFunctions/index.js
@@ -0,0 +1,23 @@
+// var a = 5;
+// var nekiText = "neki text";
+// console.log(a + 2.5);
+
+var unosVrednosti = prompt("Unesite godinu: ");
+unosVrednosti = parseInt(unosVrednosti);
+
+var unosDrugeVrednosti = prompt("Unesite godinu rodjenja: ");
+unosDrugeVrednosti = parseInt(unosDrugeVrednosti);
+
+var brojGodina = parseInt(unosVrednosti) - parseInt(unosDrugeVrednosti);
+
+var textZaConsolu = "Godine: ";
+
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+console.log(textZaConsolu + brojGodina);
+
