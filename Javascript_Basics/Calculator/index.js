2f35b2feb6c686e620dc6cdd58e54b3df82c8546
Camil Plojovic <camil.plojovic@levi9.com>
2022-05-24 19:15:40 +0200

    Added calculator

---
+++ Javascript_Basics/Calculator/index.js
@@ -0,0 +1,25 @@
+var a = prompt("Unesite prvu varijablu:");
+a = parseInt(a);
+
+var b = prompt("Unesite drugu varijablu:");
+b = parseInt(b);
+
+var operator = prompt("Unesite operator:");
+
+var mainCondition = typeof a === "number" && typeof b === "number"
+                    && !isNaN(a) && !isNaN(b);
+
+if (mainCondition){
+    var result;
+    if (operator === "-"){
+        result = a - b;
+    }
+    else if (operator === "+"){
+        result = a + b;
+    }
+    // ovde vi pisete
+
+    console.log(result);
+}
+
+
